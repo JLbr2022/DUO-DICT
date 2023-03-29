a378de1411e1589d3493222d339d1d3b743b30a4
José Freites <jlfu2004@gmail.com>
2023-03-29 11:52:56 -0300

    NavBar comp. adapted to use context

--- client/src/components/NavBar/NavBar.js
+++ client/src/components/NavBar/NavBar.js
@@ -1,10 +1,12 @@
+import { useContext } from "react";
 import { Container, Nav, Navbar } from "react-bootstrap";
 import { Button } from "react-bootstrap";
-import { useNavigate, useLocation } from "react-router-dom";
+import { useLocation } from "react-router-dom";
+import { AppContext } from "../context/appContext";
 
 export default function NavBar() {
-  const navigate = useNavigate();
   const location = useLocation();
+  const { setShow } = useContext(AppContext);
 
   const handleShowWord = () => {
     navigate("/words/w/word/asc/add");
@@ -25,26 +27,9 @@ export default function NavBar() {
               <Nav.Link href="/words/w/word/asc">Word list</Nav.Link>
               <Nav.Link href="/words/s/word/asc">Sentences list</Nav.Link>
             </Nav>
-            {(location.pathname === "/words/w/word/asc" ||
-              location.pathname === "/add") && (
-              <Button
-                variant="primary"
-                href="/words/w/word/asc/add"
-                onClick={handleShowWord}
-              >
-                Add Word
-              </Button>
-            )}
-            {(location.pathname === "/words/s/word/asc" ||
-              location.pathname === "/add") && (
-              <Button
-                variant="primary"
-                href="/words/s/word/asc/add"
-                onClick={handleShowSentences}
-              >
-                Add Sentence
-              </Button>
-            )}
+            <Button variant="primary" onClick={handleShow}>
+              {isWord ? "Add Word" : "Add Sentence"}
+            </Button>
           </Navbar.Collapse>
         </Container>
       </Navbar>
