f3b5bf067507e05d0d7765479f21c462fdc6e5c1
José Freites <jlfu2004@gmail.com>
2023-04-08 18:42:12 -0300

    @mui navBar links done. Search field  dsnt wrk sf

--- client/src/components/NavBar/NavBar.js
+++ client/src/components/NavBar/NavBar.js
@@ -1,51 +1,145 @@
-import { useContext } from "react";
-import {
-  Container,
-  Nav,
-  Navbar,
-  OverlayTrigger,
-  Tooltip,
-} from "react-bootstrap";
+import * as React from "react";
 import { AppContext } from "../context/appContext";
-import SearchEngine from "../SearchEngine/SearchEngine";
-import "./NavBar.css";
-import * as Icon from "react-bootstrap-icons";
+import { useStore } from "../context/userStore";
+import { useTranslation } from "react-i18next";
+import { styled, alpha } from "@mui/material/styles";
+import MenuIcon from "@mui/icons-material/Menu";
+import SearchIcon from "@mui/icons-material/Search";
+import {
+  Avatar,
+  AppBar,
+  Box,
+  Stack,
+  Toolbar,
+  Button,
+  IconButton,
+  InputBase,
+  Typography,
+  Grid,
+  Link,
+} from "@mui/material";
 
-export default function NavBar() {
-  const { setShow, isWord, isSentence } = useContext(AppContext);
+const Search = styled("div")(({ theme }) => ({
+  position: "relative",
+  borderRadius: theme.shape.borderRadius,
+  backgroundColor: alpha(theme.palette.common.white, 0.15),
+  "&:hover": {
+    backgroundColor: alpha(theme.palette.common.white, 0.25),
+  },
+  marginLeft: 0,
+  width: "100%",
+  [theme.breakpoints.up("sm")]: {
+    marginLeft: theme.spacing(1),
+    width: "auto",
+  },
+}));
 
-  const handleShow = () => setShow(true);
+const SearchIconWrapper = styled("div")(({ theme }) => ({
+  padding: theme.spacing(0, 2),
+  height: "100%",
+  position: "absolute",
+  pointerEvents: "none",
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "center",
+}));
 
-  const getButtonName =
-    isWord() || isSentence() ? (
-      <OverlayTrigger
-        placement="bottom"
-        overlay={<Tooltip>Add new word</Tooltip>}
-      >
-        <Icon.PlusCircle
-          className="i-add"
-          size="2rem"
-          onClick={handleShow}
-        ></Icon.PlusCircle>
-      </OverlayTrigger>
-    ) : null;
+const StyledInputBase = styled(InputBase)(({ theme }) => ({
+  color: "inherit",
+  "& .MuiInputBase-input": {
+    padding: theme.spacing(1, 1, 1, 0),
+    // vertical padding + font size from searchIcon
+    paddingLeft: `calc(1em + ${theme.spacing(4)})`,
+    transition: theme.transitions.create("width"),
+    width: "100%",
+    [theme.breakpoints.up("sm")]: {
+      width: "12ch",
+      "&:focus": {
+        width: "20ch",
+      },
+    },
+  },
+}));
+
+export default function SearchAppBar() {
+  const { isWord, isSentence } = useStore(AppContext);
+  const { t } = useTranslation();
+  const wordsURL = "http://localhost:3000/words/w/word/asc";
+  const sentencesURL = "http://localhost:3000/words/s/word/asc";
+
+  const navItems = [t("app.menu.wordsList"), t("app.menu.sentencesList")];
 
   return (
-    <>
-      <Navbar bg="dark" variant="dark" sticky="top" expand="lg">
-        <Container>
-          <Navbar.Brand href="/">Duo_Dict</Navbar.Brand>
-          <Navbar.Toggle aria-controls="basic-navbar-nav" />
-          <Navbar.Collapse id="basic-navbar-nav">
-            <Nav className="me-auto">
-              <Nav.Link href="/words/w/word/asc">Word list</Nav.Link>
-              <Nav.Link href="/words/s/word/asc">Sentences list</Nav.Link>
-            </Nav>
-            <SearchEngine />
-            {getButtonName}
-          </Navbar.Collapse>
-        </Container>
-      </Navbar>
-    </>
+    <Box>
+      <AppBar position="static">
+        <Toolbar>
+          <Grid container justifyContent="space-between" alignItems="center">
+            <Avatar sx={{ mr: 2, display: { xs: "none", sm: "flex" } }}>
+              Ddc
+            </Avatar>
+            <IconButton
+              size="large"
+              edge="start"
+              color="inherit"
+              aria-label="open drawer"
+              sx={{ mr: 2, display: { sm: "none" } }}
+            >
+              <MenuIcon />
+            </IconButton>
+
+            <Typography
+              variant="h6"
+              // noWrap
+              component="div"
+              sx={{ display: { xs: "none", sm: "block" } }}
+            >
+              <h3>{t("app.title")}</h3>
+            </Typography>
+
+            <Stack
+              direction="row"
+              spacing={1}
+              sx={{
+                display: { xs: "none", sm: "block" },
+              }}
+            >
+              {navItems.map((item) => (
+                <Button
+                  href={
+                    item === t("app.menu.wordsList") ? wordsURL : sentencesURL
+                  }
+                  key={item}
+                  sx={{ color: "#fff" }}
+                >
+                  {item}
+                </Button>
+              ))}
+              {/* {navItems.map((item) => (
+                <Button href={} key={item} sx={{ color: "#fff" }}>
+                  {item}
+                </Button>
+              ))} */}
+            </Stack>
+            <Box
+              sx={
+                {
+                  // display: { xs: 1 },
+                }
+              }
+            >
+              <Search>
+                <SearchIconWrapper>
+                  <SearchIcon />
+                </SearchIconWrapper>
+                <StyledInputBase
+                  placeholder="Search…"
+                  inputProps={{ "aria-label": "search" }}
+                />
+              </Search>
+            </Box>
+          </Grid>
+        </Toolbar>
+      </AppBar>
+    </Box>
   );
 }
