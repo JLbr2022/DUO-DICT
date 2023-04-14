cfab00e411184953032b3f4f4968113d51426d30
José Freites <jlfu2004@gmail.com>
2023-04-13 22:30:18 -0300

    Fixed Search field & button Add

--- client/src/components/NavBar/NavBar.js
+++ client/src/components/NavBar/NavBar.js
@@ -1,10 +1,12 @@
 import * as React from "react";
+import { useContext } from "react";
 import { AppContext } from "../context/appContext";
-import { useStore } from "../context/userStore";
 import { useTranslation } from "react-i18next";
-import { styled, alpha } from "@mui/material/styles";
 import MenuIcon from "@mui/icons-material/Menu";
-import SearchIcon from "@mui/icons-material/Search";
+import AddIcon from "@mui/icons-material/Add";
+import { SearchComponent } from "../SearchComponent/SearchComponent";
+import { Link as RouterLink } from "react-router-dom";
+
 import {
   Avatar,
   AppBar,
@@ -13,60 +15,18 @@ import {
   Toolbar,
   Button,
   IconButton,
-  InputBase,
   Typography,
   Grid,
-  Link,
+  Fab,
 } from "@mui/material";
 
-const Search = styled("div")(({ theme }) => ({
-  position: "relative",
-  borderRadius: theme.shape.borderRadius,
-  backgroundColor: alpha(theme.palette.common.white, 0.15),
-  "&:hover": {
-    backgroundColor: alpha(theme.palette.common.white, 0.25),
-  },
-  marginLeft: 0,
-  width: "100%",
-  [theme.breakpoints.up("sm")]: {
-    marginLeft: theme.spacing(1),
-    width: "auto",
-  },
-}));
+const wordsURL = "/words/w/word/asc";
+const sentencesURL = "/words/s/word/asc";
 
-const SearchIconWrapper = styled("div")(({ theme }) => ({
-  padding: theme.spacing(0, 2),
-  height: "100%",
-  position: "absolute",
-  pointerEvents: "none",
-  display: "flex",
-  alignItems: "center",
-  justifyContent: "center",
-}));
-
-const StyledInputBase = styled(InputBase)(({ theme }) => ({
-  color: "inherit",
-  "& .MuiInputBase-input": {
-    padding: theme.spacing(1, 1, 1, 0),
-    // vertical padding + font size from searchIcon
-    paddingLeft: `calc(1em + ${theme.spacing(4)})`,
-    transition: theme.transitions.create("width"),
-    width: "100%",
-    [theme.breakpoints.up("sm")]: {
-      width: "12ch",
-      "&:focus": {
-        width: "20ch",
-      },
-    },
-  },
-}));
-
-export default function SearchAppBar() {
-  const { isWord, isSentence } = useStore(AppContext);
+export default function NavBar() {
   const { t } = useTranslation();
-  const wordsURL = "http://localhost:3000/words/w/word/asc";
-  const sentencesURL = "http://localhost:3000/words/s/word/asc";
-
+  const { setShow } = useContext(AppContext);
+  const handleShow = () => setShow(true);
   const navItems = [t("app.menu.wordsList"), t("app.menu.sentencesList")];
 
   return (
@@ -91,7 +51,7 @@ export default function SearchAppBar() {
               variant="h6"
               // noWrap
               component="div"
-              sx={{ display: { xs: "none", sm: "block" } }}
+              sx={{ display: { xs: "none", sm: "block" }, flexGrow: 1 }}
             >
               <h3>{t("app.title")}</h3>
             </Typography>
@@ -105,39 +65,24 @@ export default function SearchAppBar() {
             >
               {navItems.map((item) => (
                 <Button
-                  href={
+                  to={
                     item === t("app.menu.wordsList") ? wordsURL : sentencesURL
                   }
                   key={item}
+                  component={RouterLink}
                   sx={{ color: "#fff" }}
                 >
                   {item}
                 </Button>
               ))}
-              {/* {navItems.map((item) => (
-                <Button href={} key={item} sx={{ color: "#fff" }}>
-                  {item}
-                </Button>
-              ))} */}
             </Stack>
-            <Box
-              sx={
-                {
-                  // display: { xs: 1 },
-                }
-              }
-            >
-              <Search>
-                <SearchIconWrapper>
-                  <SearchIcon />
-                </SearchIconWrapper>
-                <StyledInputBase
-                  placeholder="Search…"
-                  inputProps={{ "aria-label": "search" }}
-                />
-              </Search>
+            <Box>
+              <SearchComponent />
             </Box>
           </Grid>
+          <Fab size="small" aria-label="add" onClick={handleShow}>
+            <AddIcon />
+          </Fab>
         </Toolbar>
       </AppBar>
     </Box>
