d1612786f086f15f145fce6a79d0fbbeb00d717d
José Freites <jlfu2004@gmail.com>
2023-04-28 00:11:16 -0300

    ADD Footer Comp + DropDown EN/ES Lang

--- client/src/components/Footer/Footer.js
+++ client/src/components/Footer/Footer.js
@@ -1,32 +1,40 @@
 import React, { useState } from "react";
-import { Button, Switch } from "@mui/material";
+import { Box } from "@mui/material";
 import { useTranslation } from "react-i18next";
+import { FormControl, MenuItem, Select } from "@mui/material";
 
 export function Footer() {
-  const { t, i18n } = useTranslation();
-  const [checked, setChecked] = useState(true);
+  const { i18n } = useTranslation();
+  const [language, setLanguage] = useState("en");
 
-  const handleLanguage = (lang) => {
-    i18n.changeLanguage(lang);
-    localStorage.setItem("lang", lang);
-  };
+  function handleLanguage(event) {
+    const lang = event.target.value;
+    i18n.changeLanguage(lang).then(() => {
+      setLanguage(lang);
+      localStorage.setItem("lang", lang);
+    });
+  }
 
   return (
-    <>
-      <Button
-        onClick={() => {
-          handleLanguage("en");
-        }}
-      >
-        EN
-      </Button>
-      <Button
-        onClick={() => {
-          handleLanguage("es");
-        }}
-      >
-        ES
-      </Button>
-    </>
+    <Box
+      sx={{
+        display: "flex",
+        justifyContent: "right",
+        alignItems: "center",
+        width: "100%",
+        height: "60px",
+        backgroundColor: "#272727",
+        color: "#fff",
+        position: "fixed",
+        bottom: 0,
+      }}
+    >
+      <FormControl sx={{ m: 1, minWidth: 120 }} size="small">
+        <Select value={language} onChange={handleLanguage}>
+          <MenuItem value="en">EN</MenuItem>
+          <MenuItem value="es">ES</MenuItem>
+        </Select>
+      </FormControl>
+    </Box>
   );
 }
